e72846799d85af603d62ed25b4fce81e3c27b446
Gita <gita@spotify.com>
2018-10-15 14:55:33 -0400

    first commit

---
+++ csv_to_sql.py
@@ -0,0 +1,20 @@
+#!/anaconda3/bin/python
+import sqlite3
+import csv
+
+csv_file=open('citibikes_status.csv','r') # open the csv data file
+next(csv_file, None) # skip the header row
+reader = csv.reader(csv_file)
+
+sql = sqlite3.connect('citibike_db2')
+cur = sql.cursor()
+
+# create the table if it doesn't already exist
+cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landMark text)''') 
+
+for row in reader:
+	cur.execute("INSERT INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+	
+csv_file.close()
+sql.commit()
+sql.close()
