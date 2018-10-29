33b157ba962152998a7b54b09c09ad89fbfedcaa
Gita <gita@spotify.com>
2018-10-29 15:28:36 -0400

    Use with statement to open csv file, fixes #7

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -3,20 +3,18 @@
 import sqlite3
 import csv
 
-csv_file=open('citibikes_status.csv','r') # Open the csv data file
-next(csv_file, None) # Skip the header row
-reader = csv.reader(csv_file)
+with open('citibikes_status.csv','r') as csv_file: # Open the csv data file
+	next(csv_file, None)  # Skip the header row
+	reader = csv.reader(csv_file)
 
-sql = sqlite3.connect('citibike_db2')
-cur = sql.cursor()
+	sql = sqlite3.connect('citibike_db2')
+	cur = sql.cursor()
 
-# Create the table if it doesn't already exist
+	# Create the table if it doesn't already exist
+	cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landMark text)''') 
 
-cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landMark text)''') 
-
-for row in reader:
-	cur.execute("INSERT INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+	for row in reader:
+		cur.execute("INSERT INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
 	
-csv_file.close()
 sql.commit()
 sql.close()
