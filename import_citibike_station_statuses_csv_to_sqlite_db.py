dfb4b91704fcb9d40dba7f60a5f4631a19dca769
Gita <gita@spotify.com>
2018-12-13 13:41:16 -0500

    Read data from standard in, fixes #10

--- import_citibike_station_statuses_csv_to_sqlite_db.py
+++ import_citibike_station_statuses_csv_to_sqlite_db.py
@@ -2,20 +2,26 @@
 
 import sqlite3
 import csv
+import sys
 
-with open('citibikes_status.csv','r') as csv_file: # Open the csv data file
-	next(csv_file, None)  # Skip the header row
-	reader = csv.reader(csv_file)
 
-	sql = sqlite3.connect('citibike_db2')
-	cur = sql.cursor()
+# with open(sys.stdin()) as csv_file: # Open the csv data file
+csv_data = sys.stdin.read().splitlines()
+#next(csv_data, None)  # Skip the header row
 
-	# Create the table if it doesn't already exist
-	cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landMark text)''') 
-	cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS station_time on citibikes_status (ExecutionTime, id)")
-	
-	for row in reader:
-		cur.execute("INSERT OR IGNORE INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+sql = sqlite3.connect('citibike_db2')
+cur = sql.cursor()
+
+# Create the table if it doesn't already exist
+cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landmark text)''') 
+cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS station_time on citibikes_status (ExecutionTime, id)")
+
+reader = csv.reader(csv_data)
+next(reader, None)
+for row in reader:
+#	print(row)
+#	break
+	cur.execute("INSERT OR IGNORE INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
 	
 sql.commit()
 sql.close()
