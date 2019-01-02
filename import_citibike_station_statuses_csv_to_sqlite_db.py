471ef734393378e9186b91861539f7226388335d
Gita <gita@spotify.com>
2019-01-02 17:59:40 -0500

    Change to read from sys.stdin line by line

--- import_citibike_station_statuses_csv_to_sqlite_db.py
+++ import_citibike_station_statuses_csv_to_sqlite_db.py
@@ -4,16 +4,15 @@ import sqlite3
 import csv
 import sys
 
-csv_data = sys.stdin.read().splitlines()
-
 with sqlite3.connect('citibike_db2') as sql:
   cur = sql.cursor()
 
-  # Create the table if it doesn't already exist
+  # Create the table if it doesn't already exist and create unique index
   cur.execute(' ' ' CREATE TABLE IF NOT EXISTS citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landmark text)''') 
   cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS station_time on citibikes_status (ExecutionTime, id)")
 
-  reader = csv.reader(csv_data)
+  reader = csv.reader(sys.stdin)
   next(reader, None) # Skip header
   for row in reader:
+  	# print(row)
 	  cur.execute("INSERT OR IGNORE INTO citibikes_status VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
