863f1103aaaa2cb3dc90356985c16beb2c7cb680
Gita <gita@spotify.com>
2019-01-03 11:33:05 -0500

    Write data to standard out, fixes #9

--- download_citibike_station_statuses_to_csv.py
+++ download_citibike_station_statuses_to_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import requests 
 import json 
+import sys
 import csv
 
 
@@ -10,19 +11,17 @@ r = requests.get("https://feeds.citibikenyc.com/stations/stations.json")
 bikes = r.json()
 
 
-def write_csv(path, fieldnames, data):
+def write_csv(fieldnames, data):
 	
-	# Write a csv file from citibike stations app json.
-	
-	with open(path, "w", newline = "") as csv_file:
-		writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-		writer.writeheader()
-		for station in data:
-			station.update({"ExecutionTime":bikes["executionTime"]})
-			writer.writerow(station)
+	# Write a csv from citibike stations app json and feed it to standard out
 
-path = "citibikes_status.csv"
+	writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+	writer.writeheader()
+	for station in data:
+		station.update({"ExecutionTime":bikes["executionTime"]})
+		writer.writerow(station)
+
 data = bikes["stationBeanList"]
 fieldnames = ["ExecutionTime","id", "stationName", "availableDocks", "totalDocks", "latitude", "longitude", "statusValue", "statusKey", "availableBikes", "stAddress1","stAddress2","city", "postalCode", "location", "altitude", "testStation", "lastCommunicationTime", "landMark"]
 
-write_csv(path, fieldnames, data)
+write_csv(fieldnames, data)
