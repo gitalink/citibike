5dfca3dc335feb1c1839274bd136c1401ce7bc2f
Gita <gita@spotify.com>
2019-04-29 17:40:37 -0400

    Change to send logging into stdout

--- import_citibike_station_statuses_csv_to_sqlite_db.py
+++ import_citibike_station_statuses_csv_to_sqlite_db.py
@@ -6,15 +6,15 @@ import sys
 import logging
 
 
-# configure logging to log in the file
-logging.basicConfig(filename='import_script.log',level=logging.DEBUG,format='%(asctime)s %(message)s')
+# configure logging 
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 
 with sqlite3.connect('citibike_db2') as sql:
   cur = sql.cursor()
   logging.info("Opened DB Connection")
 
-  # Create the table if it doesn't already exist and create unique index
+  # Create the table if it doesn't already exist
  
   try:
     cur.execute(' ' ' CREATE TABLE citibikes_status (ExecutionTime text, id text, stationName text, availableDocks integer, totalDocks integer, latitude real, longitude real, statusValue text, statusKey text, availableBikes integer, stAddress1 text, stAddress2 text, city text, postalCode text, location text, altitude text, testStation text, lastCommunicationTime text, landmark text)''') 
@@ -23,12 +23,17 @@ with sqlite3.connect('citibike_db2') as sql:
   except sqlite3.OperationalError:  
     logging.info("Table citibikes_status exists")
 
+  # Create unique index if it doesn't already exist
+
   try:
     cur.execute("CREATE UNIQUE INDEX station_time on citibikes_status (ExecutionTime, id)")
     logging.info("Created unique index on ExecutionTime and id fields")
   
   except sqlite3.OperationalError: 
     logging.info("Unique index on ExecutionTime and id exist")
+
+
+  # Read iterable csv and insert into database 
 
   reader = csv.reader(sys.stdin)
   logging.info("Starting inserting rows")
