6bcae008e0f9e3b8a79397972cc0a0662f6e20c4
Gita <gita@spotify.com>
2019-05-08 15:36:18 -0400

    Improves logging by adding more details

--- download_citibike_station_statuses_to_csv.py
+++ download_citibike_station_statuses_to_csv.py
@@ -4,23 +4,27 @@ import json
 import sys
 import csv
 import logging
+from requests.exceptions import HTTPError
 
 # configure logging 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 # Get the data from citibikenyc and change it into json 
-try:
+try: 
 	r = requests.get("https://feeds.citibikenyc.com/stations/stations.json")
-	logging.info("Got stations data feed from https://feeds.citibikenyc.com/stations/stations.json")
-
+	r.raise_for_status()
 except HTTPError as http_err:
-	logging.info (f'HTTP error occurred: {http_err}')  
-
+	logging.info(f'Not able to download data. HTTP error occurred: {http_err}')  # Python 3.6
 except Exception as err:
-	logging.info(f'Other error occurred: {err}')  
+	logging.info(f'Other error occurred: {err}')  # Python 3.6
 
-bikes = r.json()
-logging.info("Converted json into dictionary")
+
+try:
+	bikes = r.json()
+	logging.info("Converted json into dictionary")
+
+except json.decoder.JSONDecodeError:
+	logging.info("Not able to convert json into dictionary. JSONDecodeError occurred")
 
 
 def write_csv(fieldnames, data):
@@ -32,9 +36,15 @@ def write_csv(fieldnames, data):
 	for station in data:
 		station.update({"ExecutionTime": bikes["executionTime"]})
 		writer.writerow(station)
+try:
+	data = bikes["stationBeanList"]
+except NameError:
+	logging.info("Input dictionary is not defined")
 
-data = bikes["stationBeanList"]
 fieldnames = ["ExecutionTime","id", "stationName", "availableDocks", "totalDocks", "latitude", "longitude", "statusValue", "statusKey", "availableBikes", "stAddress1","stAddress2","city", "postalCode", "location", "altitude", "testStation", "lastCommunicationTime", "landMark"]
 
-write_csv(fieldnames, data)
-logging.info("Finished writing csv")
+try:
+	write_csv(fieldnames, data)
+	logging.info("CSV writer completed successfully")
+except NameError:
+	logging.info("Input data for write_csv function is not defined")
